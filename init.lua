60fdd732f8d82c609c2cce9674c666fffe171f19
-akku- <akkun11.open@gmail.com>
2025-04-08 18:28:23 +0900

    fix layout shift

--- init.lua
+++ init.lua
@@ -2,6 +2,11 @@ require("config.lazy")
 
 vim.o.number = true
 -- vim.o.numberwidth = 5
+
+-- do not make layout shift with gitsigns
+-- https://www.reddit.com/r/neovim/comments/xjakdy/neovim_gitsigndiagnostic_icons_causes_layout_shift/
+vim.o.signcolumn = "yes"
+
 vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
