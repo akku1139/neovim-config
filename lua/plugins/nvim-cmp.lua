8b07f9bfd7a60702f44b17d21361e43c5a3a3530
-akku- <akkun11.open@gmail.com>
2025-04-08 17:52:13 +0900

    set cmp keybinds

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -16,17 +16,15 @@ return {
         { name = "buffer" },
         { name = "path" },
       },
-      --[[
       mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
-      ]]
     }
   end
 }
