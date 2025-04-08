622b11234676cd6c19d90ecfd5d9db07068fa435
-akku- <akkun11.open@gmail.com>
2025-04-08 16:13:52 +0900

    lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,16 +1,42 @@
+local dir = require("utils.dir")
+
 local l = require'lspconfig'
 
+local node_path = dir.get_data_path("/lsp/node")
+local node_modules_path = node_path .. "/node_modules/"
+
+-- TODO: Run the actual install command when using ls
+local function use_pnpm(config, pkg)
+  -- install packages
+  for _index, value in ipairs(pkg) do
+    local stat = vim.uv.fs_lstat(node_modules_path .. value)
+    if not stat then -- and stat.type == "directory"
+      -- https://www.reddit.com/r/neovim/comments/y2by27/is_there_a_way_to_run_terminal_commands_with_lua/
+      print("installing npm package: " .. value)
+      vim.system { "pnpm", "-C", node_path, "install", value }
+    end
+  end
+
+  local newcmd = vim.tbl_deep_extend("keep", config.config_def.default_config.cmd, {})
+  newcmd[1] = node_modules_path .. "/.bin/" .. newcmd[1]
+
+  config.setup {
+    cmd = newcmd
+  }
+end
+
 --- TypeScript
--- npm: typescript typescript-language-server
-l.ts_ls.setup{}
+use_pnpm(l.ts_ls, { "typescript", "typescript-language-server" })
 
 --- Svelte
 -- npm: svelte-language-server
-l.svelte.setup{}
+-- l.svelte.setup{}
 -- let g： svelte_indent_script = 0
 -- let g： svelte_indent_style = 0 
 
 --- Lua
 -- termux-apt: lua-language-server
-l.lua_ls.setup{}
+-- l.lua_ls.setup{}
 
+-- Python
+use_pnpm(l.pyright, { "pyright" })
---
+++ lua/utils/dir.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+-- TODO: Automatically create parent directory
+
+M.create_directory_if_symlink_target_not_exist = function(p)
+  local is_symlink = vim.uv.fs_lstat(p) and vim.uv.fs_lstat(p).type == "link"
+
+  if is_symlink then
+    local target_path = vim.uv.fs_readlink(p)
+    local target_stat = vim.uv.fs_stat(target_path)
+
+    if not target_stat then
+      local ok, err = vim.uv.fs_mkdir(target_path, tonumber("700", 8))
+      if not ok then
+        vim.notify("failed to mkdir: " .. err, vim.log.levels.ERROR)
+      end
+    end
+  end
+end
+
+M.get_data_path = function(p)
+  local path = vim.fn.stdpath("data") .. p
+  M.create_directory_if_symlink_target_not_exist(path)
+  return path
+end
+
+return M
