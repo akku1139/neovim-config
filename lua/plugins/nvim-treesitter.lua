972eb8191bd74802efeb09dff19c08059ffa2dca
-akku- <akkun11.open@gmail.com>
2025-04-15 20:47:08 +0900

    fix treesitter auto install

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,6 +3,9 @@
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
+parser_install_dir = vim.fn.stdpath("data") .. "/treesitter"
+vim.opt.runtimepath:append(parser_install_dir)
+
 return {
   "nvim-treesitter/nvim-treesitter",
   main = 'nvim-treesitter.configs', -- important
@@ -12,6 +15,6 @@ return {
     highlight = { enable = true },
     indent = { enable = true },
     auto_install = false,
-    parser_install_dir = vim.fn.stdpath("data") .. "/treesitter"
+    parser_install_dir = parser_install_dir,
   }, 
 }
