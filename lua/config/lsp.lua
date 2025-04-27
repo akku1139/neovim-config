325fb835c08da55cc90add62ca3cd23d07c3c8c1
-akku- <akkun11.open@gmail.com>
2025-04-28 03:05:19 +0900

    fix

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,12 +2,15 @@ local dir = require("utils.dir")
 
 local l = require("lspconfig")
 
-local node_path = vim.fn.stdpath("data") .. "/lsp/node"
-if vim.uv.fs_stat(node_path .. "package.json") then
-  vim.system { "pnpm", "-C", node_path, "install" }
-  dir.get_data_path(node_path .. "/node_modules/")
-end
-local node_modules_path = dir.get_data_path(node_path .. "/node_modules/")
+local node_path = dir.get_data_path("/lsp/node")
+local node_modules_path = node_path .. "/node_modules/"
+
+-- pnpmがnode_modulesがsymlinkだとリンク先を破壊する(?)
+--local node_path = vim.fn.stdpath("data") .. "/lsp/node"
+--local node_modules_path = dir.get_data_path("/lsp/node/node_modules")
+--if vim.uv.fs_stat(node_path .. "package.json") then
+--  vim.system { "pnpm", "-C", node_path, "install" }
+--end
 
 local function use_pnpm(config, pkg)
   --- install packages
