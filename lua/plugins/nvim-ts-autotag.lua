6a6d3b04f285b5e5fb7654996b5abe8e9e669eca
-akku- <akkun11.open@gmail.com>
2025-05-26 17:22:45 +0900

    fix

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -2,8 +2,10 @@ return {
   "windwp/nvim-ts-autotag",
   event = "InsertEnter",
   opts = {
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = true -- Auto close on trailing </
+    opts = {
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = true -- Auto close on trailing </
+    },
   },
 }
